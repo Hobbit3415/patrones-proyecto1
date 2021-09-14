c3360847763c2e7343efa81d701780f512c4f0f4
Hobbit3415 <emmanuel.mora3415@gnail.com>
2021-09-13 20:14:32 -0500

    Cambio de nombre del launcher

--- bancoPDUSA/src/launcher/LauncherFactoryTarjetas.java
+++ bancoPDUSA/src/launcher/LauncherFactoryTarjetas.java
@@ -6,7 +6,7 @@ import factory.VisaFactory;
 import producto.ITarjeta;
 import producto.TarjetaCredito;
 
-public class Launcher {
+public class LauncherFactoryTarjetas {
   
   public static void main(String[] args) {
     System.out.print("\033[H\033[2J");
