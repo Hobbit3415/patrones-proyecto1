ddde8fca85b07c95548de0cd3abed6b1c00a74f0
valentinafranco <valenf2903@hotmail.com>
2021-09-14 20:04:21 -0500

    Modulo validacion tarjeta implementado

---
+++ bancoPDUSA/src/launcher/launcher.java
@@ -0,0 +1,18 @@
+package launcher;
+
+import java.util.Scanner;
+
+public class launcher {
+	public static void main(String[] args) {
+	
+	    Scanner sc = new Scanner(System.in);
+	    long numero;
+	    
+	    System.out.println("Ingrese el número de la tarjeta: ");
+	    numero = sc.nextLong();
+	    ValidacionTarjeta v = ValidacionTarjeta.getValidacionTarjeta(numero);
+	    System.out.println("\n");
+	    
+	  }
+}
+//3379513561108795
